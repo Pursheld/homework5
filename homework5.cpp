1714c8742558a34679495e9d7a72354260de9355
Pursheld <stompee@bk.ru>
2021-04-26 20:33:13 +0300

    Final

--- homework5.cpp
+++ homework5.cpp
@@ -4,13 +4,24 @@
 class Vector
 {
 public:
+	
 	Vector() : x(5), y(5), z(5)
 	{}
 	void showXYZ()
 	{
-		std::cout << "\n" << x << " " << y << " " << z << "\n";
+		std::cout << "\n Enter X: ";
+		std::cin >> x;
+
+		std::cout << "\n Enter Y: ";
+		std::cin >> y;
+
+		std::cout << "\n Enter Z: ";
+		std::cin >> z;
+
+		std::cout << "\n" << "x = "  <<  x << " " << "y = " << y << " " << "z = " << z << "\n";
 		lenVector = sqrt(x * x + y * y + z * z);
-		std::cout << lenVector;
+		std::cout << "\n" << "vector length ="<< lenVector;
+		
 	}
 private:
 	double x;
@@ -22,6 +33,8 @@ private:
 int main()
 {
 	Vector v;
+	
 	v.showXYZ();
+	
 }
 
