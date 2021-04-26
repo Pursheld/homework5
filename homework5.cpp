d8606ef27afd492238a6c8490cbd9400bcf18a80
Pursheld <stompee@bk.ru>
2021-04-26 18:52:15 +0300

    Добавьте файлы проекта.

---
+++ homework5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+class Vector
+{
+public:
+	Vector() : x(5), y(5), z(5)
+	{}
+	void showXYZ()
+	{
+		std::cout << "\n" << x << " " << y << " " << z << "\n";
+		lenVector = sqrt(x * x + y * y + z * z);
+		std::cout << lenVector;
+	}
+private:
+	double x;
+	double y;
+	double z;
+	double  lenVector;
+};
+
+int main()
+{
+	Vector v;
+	v.showXYZ();
+}
+
